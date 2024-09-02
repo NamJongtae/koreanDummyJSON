0d0588078b137335a52fda86a845219d5dc7379e
MAIN6419 <dark9737@gmail.com>
2024-09-02 21:05:10 +0900

    Feat: posts 하위 리소스 comments 조회 api router 생성

---
+++ app/api/posts/[id]/comments/route.ts
@@ -0,0 +1,26 @@
+import executeQuery from "@/src/db/db";
+import { NextRequest, NextResponse } from "next/server";
+
+interface IParams {
+  params: { id: string };
+}
+export async function GET(req: NextRequest, { params }: IParams) {
+  try {
+    const id = params.id;
+
+    const sql =
+      "SELECT posts.id, comments.id AS commentId, comments.content, comments.createdAt FROM posts INNER JOIN comments ON posts.id = comments.postId where posts.id = ?;";
+    const comments = (await executeQuery(sql, [id])) as Comment[];
+
+    return NextResponse.json(
+      { message: "게시물 댓글 목록 조회 성공", comments },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "게시물 댓글 목록 조회 실패" },
+      { status: 500 }
+    );
+  }
+}
