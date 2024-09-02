c64dbc28239e46e8c600690f7596feabbf2cf6cb
MAIN6419 <dark9737@gmail.com>
2024-09-02 22:06:38 +0900

    Feat: books 하위 리소스 reviews 조회 api router 생성

---
+++ app/api/books/[id]/reviews/route.ts
@@ -0,0 +1,27 @@
+import executeQuery from "@/src/db/db";
+import { Review } from "@/src/types/review-type";
+import { NextRequest, NextResponse } from "next/server";
+
+interface IParams {
+  params: { id: string };
+}
+export async function GET(req: NextRequest, { params }: IParams) {
+  try {
+    const id = params.id;
+
+    const sql =
+      "SELECT books.id, reviews.id AS reviewId, reviews.rating, reviews.content, reviews.createdAt FROM books INNER JOIN reviews ON books.id = reviews.bookId where books.id = ?;";
+    const reviews = (await executeQuery(sql, [id])) as Review[];
+
+    return NextResponse.json(
+      { message: "책 리뷰 목록 조회 성공", reviews },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "책 리뷰 목록 조회 실패" },
+      { status: 500 }
+    );
+  }
+}
