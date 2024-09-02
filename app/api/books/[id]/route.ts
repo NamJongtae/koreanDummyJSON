f3ec1e9a7012a2916e2c0ac3688c0398658acf36
MAIN6419 <dark9737@gmail.com>
2024-09-02 21:40:34 +0900

    Feat: books GET, POST, PUT PATCH, DELETE api router 생성

---
+++ app/api/books/[id]/route.ts
@@ -0,0 +1,123 @@
+import executeQuery from "@/src/db/db";
+import { Book } from "@/src/types/book-type";
+
+import { NextRequest, NextResponse } from "next/server";
+
+interface IParams {
+  params: {
+    id: string;
+  };
+}
+
+// /api/books/:id
+export async function GET(req: NextRequest, { params }: IParams) {
+  try {
+    const id = params.id;
+
+    const sql = "SELECT * FROM books where id = ?";
+    const books = (await executeQuery(sql, [id])) as Book[];
+
+    if (books.length === 0) {
+      return NextResponse.json(
+        { message: "책이 존재하지 않습니다. id 값을 확인해주세요." },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "책 조회 성공", book: books[0] },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "책 조회 실패" }, { status: 500 });
+  }
+}
+
+export async function PUT(req: NextRequest, { params }: IParams) {
+  const id = params.id;
+  const { author, genre, title, publicationDate, totalPage } = await req
+    .json()
+    .catch(() => ({}));
+
+  try {
+    // 데이터베이스에서 실제 데이터를 조회
+    const books = (await executeQuery("SELECT * FROM books WHERE id = ?", [
+      id
+    ])) as Book[];
+
+    if (books.length === 0) {
+      return NextResponse.json(
+        { message: "책이 존재하지 않습니다. id 값을 확인해주세요." },
+        { status: 404 }
+      );
+    }
+
+    // 더미 데이터를 만듭니다 (실제 DB 수정 대신)
+    const dummyData = {
+      id: books[0].id,
+      author,
+      genre,
+      title,
+      publicationDate: new Date(publicationDate).toISOString(),
+      totalPage
+    };
+
+    return NextResponse.json(
+      {
+        message: "책 수정 성공",
+        book: dummyData
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function PATCH(req: NextRequest, { params }: IParams) {
+  const id = params.id;
+  const { author, genre, title, publicationDate, totalPage } = await req
+    .json()
+    .catch(() => ({}));
+
+  try {
+    // 데이터베이스에서 실제 데이터를 조회
+    const books = (await executeQuery("SELECT * FROM books WHERE id = ?", [
+      id
+    ])) as Book[];
+
+    if (books.length === 0) {
+      return NextResponse.json(
+        { message: "책이 존재하지 않습니다. id 값을 확인해주세요." },
+        { status: 404 }
+      );
+    }
+
+    // 더미 데이터를 만듭니다 (실제 DB 수정 대신)
+    const dummyData = {
+      ...books[0],
+      ...(author !== undefined && { author }),
+      ...(genre !== undefined && { genre }),
+      ...(title !== undefined && { title }),
+      ...(publicationDate !== undefined && {
+        publicationDate: new Date(publicationDate).toISOString()
+      }),
+      ...(totalPage !== undefined && { totalPage })
+    };
+
+    return NextResponse.json(
+      {
+        message: "책 수정 성공",
+        book: dummyData
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  return NextResponse.json({ message: "책 삭제 성공" }, { status: 200 });
+}
---
+++ app/api/books/route.ts
@@ -0,0 +1,93 @@
+import executeQuery from "@/src/db/db";
+import { Book } from "@/src/types/book-type";
+import { NextRequest, NextResponse } from "next/server";
+
+// /api/books
+export async function GET(req: NextRequest) {
+  try {
+    const searchParams = req.nextUrl.searchParams;
+
+    // page와 limit 값을 가져옵니다.
+    const page = searchParams.get("page");
+    const limit = searchParams.get("limit");
+
+    let sql;
+    let values: Array<number> = [];
+    let offset: number | null = null;
+
+    if (page && limit) {
+      // 페이지네이션 계산
+      offset = (parseInt(page) - 1) * parseInt(limit);
+
+      // SQL 쿼리에 LIMIT과 OFFSET 적용
+      // /api/books?page={page}&limit={limit} 책 목록 페이지
+      sql = "SELECT * FROM books LIMIT ? OFFSET ?";
+      values = [parseInt(limit), offset];
+    } else {
+      // page 또는 limit가 없으면 전체 데이터를 조회
+      // /api/posts 책 목록
+      sql = "SELECT * FROM books";
+    }
+
+    // 데이터베이스 쿼리 실행
+    const data = await executeQuery(sql, values);
+
+    // hasNextPage 계산
+    const totalBooks = 200;
+    const hasNextPage =
+      offset !== null && offset + parseInt(limit || "0") < totalBooks;
+
+    // 응답 객체 생성
+    const response: {
+      message: string;
+      books: Book[];
+      page?: number;
+      limit?: number;
+      hasNextPage?: boolean;
+    } = {
+      message: "책 목록 조회 성공",
+      books: data as Book[]
+    };
+
+    // 조건에 따라 page, limit, hasNextPage 추가
+    if (page) response.page = parseInt(page);
+    if (limit) response.limit = parseInt(limit);
+    if (hasNextPage !== null) response.hasNextPage = hasNextPage;
+
+    return NextResponse.json(response, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "책 목록 조회 실패" }, { status: 500 });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const {
+    author = "",
+    genre = "",
+    title = "",
+    publicationDate = "",
+    totalPage = 0
+  } = await req.json().catch(() => ({}));
+
+  try {
+    // 더미 데이터를 만듭니다 (실제 DB 수정 대신)
+    const dummyData = {
+      id: 101,
+      author,
+      genre,
+      title,
+      publicationDate,
+      totalPage
+    };
+    return NextResponse.json(
+      {
+        message: "책 생성 성공",
+        book: dummyData
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
