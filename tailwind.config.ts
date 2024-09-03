bd98b7c5523cf0d84352bff168713ead18f25d80
MAIN6419 <dark9737@gmail.com>
2024-09-03 18:49:44 +0900

    Chore: tailwind.config 설정 추가 - src 경로 추가 - 반응형 screens size 추가 - animation 추가 - 모바일 hover를 막는 betterhover 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,48 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaulttheme";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    screens: {
+      xxs: "320px",
+      xs: "486px",
+      ...defaultTheme.screens
     },
+    extend: {
+      screens: {
+        betterhover: { raw: "(hover: hover)" }
+      },
+      keyframes: {
+        fadeInUp: {
+          "0%": { transform: "scale(0.95)", opacity: "0" },
+          "100%": { transform: "scale(1.0)", opacity: "1" }
+        },
+        fadeOutDown: {
+          "0%": { transform: "scale(1.0)", opacity: "1" },
+          "100%": { transform: "scale(0.95)", opacity: "0" }
+        },
+        slideOutRight: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(100%)" }
+        },
+        slideOutLeft: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0)" }
+        }
+      },
+      animation: {
+        entering: "fadeInUp 200ms ease-out",
+        leaving: "fadeOutDown 200ms ease-in",
+        slideOutRight: "slideOutRight 0.5s ease-in-out",
+        slideOutLeft: "slideOutLeft 0.5s"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
