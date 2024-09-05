380efd5dbe7812bfcc3ceaf891cb55759f57b7fe
MAIN6419 <dark9737@gmail.com>
2024-09-05 20:10:58 +0900

    Feat: Fetch Section 공통 컴포넌트 생성 - fetch api 예시 코드 출력, 실행 및 결과를 출력하는 Section 컴포넌트

---
+++ src/components/commons/fetch-section/fetch-section.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import CodeSnippet from "./code-snippet";
+import FetchButton from "./fetch-button";
+import CopyBtn from "./copy-btn";
+import FetchResult from "./fetch-result";
+import useFetch from "@/src/hooks/commons/useFetch";
+import { generateCodeSnippet } from "@/src/lib/generateCodeSnippet";
+
+interface IProps {
+  sectionRef?: React.Ref<HTMLDivElement>;
+  id?: string;
+  title: string;
+  endpoint?: string;
+  method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+  body?: object;
+  descriptions: React.ReactElement;
+  fetchUrl: string;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export default function FetchSection({
+  sectionRef,
+  id,
+  title,
+  endpoint,
+  method,
+  body,
+  descriptions,
+  fetchUrl,
+  className,
+  children
+}: IProps) {
+  const { data, fetchData, isLoading } = useFetch();
+
+  const code = generateCodeSnippet(fetchUrl, method, body);
+
+  return (
+    <section
+      ref={sectionRef}
+      id={id}
+      className={`pt-20 px-6 ${className && className}`}
+    >
+      <h2 className="section-title">{title}</h2>
+      {endpoint && (
+        <p className="section-text mb-2">
+          <span className="font-medium">Endpoint</span> : {endpoint}
+        </p>
+      )}
+      {endpoint && (
+        <p className="section-text mb-2">
+          <span className="font-medium">Method</span> : {method}
+        </p>
+      )}
+
+      {descriptions}
+
+      <CodeSnippet code={code} />
+
+      <FetchButton
+        fetchData={() => fetchData(`/api${fetchUrl}`, method, body)}
+      />
+
+      <CopyBtn target={code} />
+
+      <FetchResult data={data} isLoading={isLoading} />
+
+      {children}
+    </section>
+  );
+}
