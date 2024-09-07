159041bb9c976df82a52333f6a2adbd0c0e74daa
MAIN6419 <dark9737@gmail.com>
2024-09-07 22:46:32 +0900

    Feat: metadata verification google, naver 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,12 @@ export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL as string),
   description: `한국어로 구성된 데이터를 통해 개발자들이 보다 현실적인 더미 데이터를 제공 받을 수 있습니다. 
     GET, POST, PUT, PATCH, DELETE 요청을 보내고 직접 테스트 해보고 학습해보세요.`,
+  verification: {
+    google: "-LptG2gzye6iVIM8aZT5fiJGQqrMVVQVi2lpo1wcPv4",
+    other: {
+      "naver-site-verification": "113a089154e96d7d1603d689bb25f8ac2ac5f289"
+    }
+  },
   icons: {
     icon: "/icons/logo.svg"
   },
