1ed6b5189d1aed786aa55ed326b047106016aaa7
MAIN6419 <dark9737@gmail.com>
2024-09-08 21:07:52 +0900

    Fix: middleware matcher 설정 수정

--- middleware.ts
+++ middleware.ts
@@ -15,17 +15,11 @@ export async function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    "/api/users",
     "/api/users/:path*",
-    "/api/todos",
     "/api/todos/:path*",
-    "/api/posts",
     "/api/posts/:path*",
-    "/api/comments",
     "/api/comments/:path*",
-    "/api/books",
     "/api/books/:path*",
-    "/api/reviews",
     "/api/reviews/:path*"
   ]
 };
