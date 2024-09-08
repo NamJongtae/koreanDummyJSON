0f3e96bd2a5651c7a23ecfd0a1fdbc3460d07230
MAIN6419 <dark9737@gmail.com>
2024-09-08 14:08:09 +0900

    Chore: metadata keywords 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL as string),
   description: `한국어로 구성된 데이터를 통해 개발자들이 보다 현실적인 더미 데이터를 제공 받을 수 있습니다. 
     GET, POST, PUT, PATCH, DELETE 요청을 보내고 직접 테스트 해보고 학습해보세요.`,
+    keywords: "korean JSON, placeholder json, korean placeholder json, dummy json, korean dummy json, 더미 데이터, API 테스트, GET, POST, PUT, PATCH, DELETE",
   verification: {
     google: "-LptG2gzye6iVIM8aZT5fiJGQqrMVVQVi2lpo1wcPv4",
     other: {
