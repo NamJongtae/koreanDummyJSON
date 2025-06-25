783f54cbd5e774d649f75d60df34cd0732dcb9db
MAIN6419 <dark9737@gmail.com>
2025-06-25 14:26:38 +0900

    Chore: jest.config 설정 파일 생성

---
+++ jest.config.cjs
@@ -0,0 +1,18 @@
+const nextJest = require("next/jest"); 
+
+const createJestConfig = nextJest({
+  dir: "./" // Next.js 앱의 루트 디렉토리 설정
+});
+
+const customJestConfig = {
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1" // 절대 경로 매핑
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest" // TypeScript 파일 인식
+  },
+  extensionsToTreatAsEsm: [".ts", ".tsx"]
+};
+
+module.exports = createJestConfig(customJestConfig);
