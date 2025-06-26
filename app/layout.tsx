1e975fcfba4be631278b725fde4eb4070df75fcc
MAIN6419 <dark9737@gmail.com>
2025-06-26 17:41:08 +0900

    Fix: Layout 컴포넌트 openGraph 옵션 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,8 @@ export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL as string),
   description: `한국어로 구성된 데이터를 통해 개발자들이 보다 현실적인 더미 데이터를 제공 받을 수 있습니다. 
     GET, POST, PUT, PATCH, DELETE 요청을 보내고 직접 테스트 해보고 학습해보세요.`,
-    keywords: "korean JSON, placeholder json, korean placeholder json, dummy json, korean dummy json, 더미 데이터, API 테스트, GET, POST, PUT, PATCH, DELETE",
+  keywords:
+    "korean JSON, placeholder json, korean placeholder json, dummy json, korean dummy json, 더미 데이터, API 테스트, GET, POST, PUT, PATCH, DELETE",
   verification: {
     google: "-LptG2gzye6iVIM8aZT5fiJGQqrMVVQVi2lpo1wcPv4",
     other: {
@@ -29,8 +30,14 @@ export const metadata: Metadata = {
     description: `한국어로 구성된 데이터를 통해 개발자들이 보다 현실적인 더미 데이터를 제공 받을 수 있습니다. 
     GET, POST, PUT, PATCH, DELETE 요청을 보내고 직접 테스트 해보고 학습해보세요.`,
     images: {
-      url: "/icons/logo-icon.svg"
-    }
+      url: "/images/og-img.jpg",
+      width: 1200,
+      height: 630,
+      alt: "Korean Dummy JSON"
+    },
+    siteName: "Korean Dummy JSON",
+    locale: "ko-KR",
+    type: "website"
   }
 };
 
