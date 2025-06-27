18a5b2343cf6457255f3522135f49eb9af522c63
MAIN6419 <dark9737@gmail.com>
2025-06-27 14:01:19 +0900

    Refactor: getdb 함수 싱글톤 패턴 제거 - 싱글톤으로 하나의 DB 인스턴스를 공유 시 이전 작업이 끝나기 전까지 락이 걸려 대기해야하는 문제가 있어 응답 속도 감소 - 전역 DB 인스턴스 사용을 중단하고 매 호출 시 새로운 연결을 생성하도록 수정

--- src/db/sqlite.ts
+++ src/db/sqlite.ts
@@ -2,15 +2,10 @@ import sqlite3 from "sqlite3";
 import { open, Database as SqliteDatabase } from "sqlite";
 import path from "path";
 
-let db: SqliteDatabase | null = null;
-
 export async function getDb(): Promise<SqliteDatabase> {
-  if (!db) {
-    const DB_PATH = path.join(process.cwd(), "src/db/app.db");
-    db = await open({
-      filename: DB_PATH,
-      driver: sqlite3.Database
-    });
-  }
-  return db;
+  const DB_PATH = path.join(process.cwd(), "src/db/app.db");
+  return await open({
+    filename: DB_PATH,
+    driver: sqlite3.Database
+  });
 }
