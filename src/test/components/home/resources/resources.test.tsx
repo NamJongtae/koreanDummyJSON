227c936467802befee103eb8740cb8d8db288131
MAIN6419 <dark9737@gmail.com>
2025-07-01 14:41:13 +0900

    Fix: Resources 컴포넌트 resource-data 렌더링 확인 단위 테스트 코드 수정 - resource-data를 모킹 하지 않고 실제 전체 resource-data가 렌더링되는 확인하도록 수정

--- src/test/components/home/resources/resources.test.tsx
+++ src/test/components/home/resources/resources.test.tsx
@@ -14,12 +14,6 @@ jest.mock("@/src/components/home/resources/resources-rows", () => ({
     </tr>
   )
 }));
-jest.mock("@/src/table-data/resources-data", () => ({
-  RESOURCES_DATA: [
-    { resource: "/users", info: "유저 목록 반환" },
-    { resource: "/posts", info: "게시글 목록 반환" }
-  ]
-}));
 
 describe("Resources 컴포넌트", () => {
   const mockRef = { current: null };
@@ -39,7 +33,7 @@ describe("Resources 컴포넌트", () => {
     // 설명
     expect(
       screen.getByText(
-        /users, posts, comments, todos, books, reviews, auth, image 8개의 리소스가 제공됩니다/
+        /users, posts, comments, todos, books, reviews, auth, image, lorem 9개의 리소스가 제공됩니다/
       )
     ).toBeInTheDocument();
     // 테이블 헤더
@@ -47,11 +41,25 @@ describe("Resources 컴포넌트", () => {
     expect(screen.getByText("Information")).toBeInTheDocument();
     // rows
     const rows = screen.getAllByTestId("mock-row");
-    expect(rows.length).toBe(2);
+    expect(rows.length).toBe(9);
     expect(rows[0]).toHaveTextContent("/users");
-    expect(rows[0]).toHaveTextContent("유저 목록 반환");
+    expect(rows[0]).toHaveTextContent("유저 20명");
     expect(rows[1]).toHaveTextContent("/posts");
-    expect(rows[1]).toHaveTextContent("게시글 목록 반환");
+    expect(rows[1]).toHaveTextContent("게시물 100개");
+    expect(rows[2]).toHaveTextContent("/comments");
+    expect(rows[2]).toHaveTextContent("댓글 500개");
+    expect(rows[3]).toHaveTextContent("/todos");
+    expect(rows[3]).toHaveTextContent("할 일 200개");
+    expect(rows[4]).toHaveTextContent("/books");
+    expect(rows[4]).toHaveTextContent("책 100개");
+    expect(rows[5]).toHaveTextContent("/reviews");
+    expect(rows[5]).toHaveTextContent("리뷰 500개");
+    expect(rows[6]).toHaveTextContent("/auth");
+    expect(rows[6]).toHaveTextContent("로그인 및 인증/인가");
+    expect(rows[7]).toHaveTextContent("/image");
+    expect(rows[7]).toHaveTextContent("동적 이미지 생성");
+    expect(rows[8]).toHaveTextContent("/lorem");
+    expect(rows[8]).toHaveTextContent("한글 로렘 입숨 생성");
   });
 
   it("isVisible 값에 따라 opacity/translate 클래스가 올바르게 적용된다", () => {
