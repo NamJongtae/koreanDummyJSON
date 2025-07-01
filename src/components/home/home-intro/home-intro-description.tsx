b79cf720b3833aa50000620a85368120c6348110
MAIN6419 <dark9737@gmail.com>
2025-07-01 14:23:20 +0900

    Fix: HomeIntroDescription 컴포넌트 한글 로렘 입숨 기능 설명 추가

--- src/components/home/home-intro/home-intro-description.tsx
+++ src/components/home/home-intro/home-intro-description.tsx
@@ -22,6 +22,11 @@ export default function HomeIntroDescription() {
         추가로 JWT 기반 로그인 및 인증/인가 더미 API 및 동적 더미 이미지 API가
         제공됩니다.
       </p>
+      <p className="text-base sm:text-lg md:text-xl mb-3 text-gray-500 whitespace-pre-line">
+        {
+          "한글 로렘 입숨 API가 새롭게 추가되었습니다.\nAPI를 직접 사용하지 않아도 LOREM 페이지에서 한글 로렘 입숨을 바로 만들 수 있습니다.\n문단, 문장, 단어 모드를 선택하고 원하는 개수와 글자 수를 지정해 한글 로렘 입숨을 생성해보세요."
+        }
+      </p>
       <p className="text-base sm:text-lg md:text-xl mb-3 text-gray-500">
         GET, POST, PUT, PATCH, DELETE 요청을 보내고 직접 테스트 해보고 학습해
         보세요.
