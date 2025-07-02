30e86bf5bb7857d0ea091a85afd6f67963e29138
MAIN6419 <dark9737@gmail.com>
2025-07-02 17:11:22 +0900

    Fix: useResetLorem customhook LoremButtonGroup -> ResetButton 컴포넌트로 이동 - ResetButton 컴포넌트 mode, resetByMode, setResult props 추가

--- src/components/lorem/lorem-button-group.tsx
+++ src/components/lorem/lorem-button-group.tsx
@@ -2,12 +2,10 @@ import React, { Dispatch, SetStateAction } from "react";
 import GenerateButton from "./generate-button";
 import CopyButton from "./copy-button";
 import ResetButton from "./reset-button";
-import useResetLorem from "@/src/hooks/lorem/useResetLorem";
 
 interface IProps {
   result: string;
   mode: "paragraph" | "sentence" | "word";
-  asHtml?: boolean;
   resetByMode: (newMode: "paragraph" | "sentence" | "word") => void;
   setResult: Dispatch<SetStateAction<string>>;
   onGenerate: () => void;
@@ -16,23 +14,20 @@ interface IProps {
 const LoremButtonGroup = ({
   result,
   mode,
-  asHtml = false,
   resetByMode,
   setResult,
   onGenerate
 }: IProps) => {
-  const { handleReset } = useResetLorem({
-    mode,
-    resetByMode,
-    setResult
-  });
-
   return (
     <div className="flex justify-between items-center mb-1 gap-2">
       <GenerateButton onGenerate={onGenerate} />
       <div className="flex gap-2">
         <CopyButton result={result} disabled={!result} />
-        <ResetButton onReset={handleReset} />
+        <ResetButton
+          mode={mode}
+          resetByMode={resetByMode}
+          setResult={setResult}
+        />
       </div>
     </div>
   );
--- src/components/lorem/reset-button.tsx
+++ src/components/lorem/reset-button.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import useResetLorem from "@/src/hooks/lorem/useResetLorem";
+import React, { Dispatch, SetStateAction } from "react";
 
 interface IProps {
-  onReset: () => void;
+  mode: "paragraph" | "sentence" | "word";
+  resetByMode: (newMode: "paragraph" | "sentence" | "word") => void;
+  setResult: Dispatch<SetStateAction<string>>;
 }
 
-const ResetButton = ({ onReset }: IProps) => (
-  <button
-    className="flex items-center gap-1 px-3 md:px-6 py-1 rounded border text-sm md:text-base font-medium transition bg-white border-gray-300 text-gray-600 hover:bg-gray-50 hover:border-gray-400 shadow-sm"
-    onClick={onReset}
-    type="button"
-  >
-    <span>⟲</span>
-    <span>리셋</span>
-  </button>
-);
+const ResetButton = ({ mode, resetByMode, setResult }: IProps) => {
+  const { handleReset } = useResetLorem({
+    mode,
+    resetByMode,
+    setResult
+  });
+
+  return (
+    <button
+      className="flex items-center gap-1 px-3 md:px-6 py-1 rounded border text-sm md:text-base font-medium transition bg-white border-gray-300 text-gray-600 hover:bg-gray-50 hover:border-gray-400 shadow-sm"
+      onClick={handleReset}
+      type="button"
+    >
+      <span>⟲</span>
+      <span>리셋</span>
+    </button>
+  );
+};
 
 export default ResetButton;
