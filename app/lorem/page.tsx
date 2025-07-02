c7f56006d66cf184ea7aef388d91d062ea7c6d7d
MAIN6419 <dark9737@gmail.com>
2025-07-03 00:21:22 +0900

    Fix: Lorem page metadata, openGraph 수정

--- app/lorem/page.tsx
+++ app/lorem/page.tsx
@@ -1,8 +1,8 @@
 import LoremPage from "@/src/components/lorem/lorem-page";
 
 export const metadata = {
-  title: "한글 Lorem Ipsum 생성기 | Korean Dummy JSON",
-  description: "한국어 더미 텍스트(문단, 문장, 단어)를 쉽고 빠르게 생성하세요.",
+  title: "한글 Lorem Ipsum 생성기",
+  description: "한국어, 한글 더미 텍스트(문단, 문장, 단어)를 쉽고 빠르게 생성하세요.",
   keywords: [
     "한글 더미 텍스트",
     "Lorem Ipsum",
@@ -17,7 +17,7 @@ export const metadata = {
     url: new URL(process.env.NEXT_PUBLIC_BASE_URL as string),
     title: "한글 Lorem Ipsum 생성기 | Korean Dummy JSON",
     description:
-      "한국어 더미 텍스트(문단, 문장, 단어)를 쉽고 빠르게 생성하세요.",
+      "한국어, 한글 더미 텍스트(문단, 문장, 단어)를 쉽고 빠르게 생성하세요.",
     images: {
       url: "/images/og-img.jpg",
       width: 1200,
