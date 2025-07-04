27ced1fce0407a6910b83a517c163af0fe177c08
MAIN6419 <dark9737@gmail.com>
2025-07-04 13:44:05 +0900

    Fix: API_ENDPOINT_DATA 수정 및 filter 메소드를 사용하여 종류별 엔드포인트 생성 - API_ENDPOINT_DATA action '페이지' -> '페이징'으로 수정 - API_ENDPOINT_DATA action '필터링' -> '조회'로 수정 - 아래와 같이 filter 메소드를 사용하여 종류별 ENDPOINT_DATA 생성   - ID_ENDPOINT_DATA, LIST_ENDPOINT_DATA, PAGING_ENDPOINT_DATA   - POST_ENDPOINT_DATA, PUT_ENDPOINT_DATA, PATCH_ENDPOINT_DATA   - DELETE_ENDPOINT_DATA, FILTER_ENDPOINT_DATA, NESTED_ENDPOINT_DATA

--- src/table-data/api-endpoints-data.ts
+++ src/table-data/api-endpoints-data.ts
@@ -11,37 +11,37 @@ export const API_ENDPOINT_DATA = [
     endpoint: "/users?page={page}&limit={limit}",
     url: "/users?page=1&limit=10",
     method: "GET",
-    action: "유저 목록 페이지"
+    action: "유저 목록 페이징"
   },
   {
     endpoint: "/users/:id/todos",
     url: "/users/1/todos",
     method: "GET",
-    action: "유저 할 일 목록 조회"
+    action: "유저별 할 일 목록 조회"
   },
   {
     endpoint: "/users/:id/posts",
     url: "/users/1/posts",
     method: "GET",
-    action: "유저 게시물 목록 조회"
+    action: "유저별 게시물 목록 조회"
   },
   {
     endpoint: "/users/:id/comments",
     url: "/users/1/comments",
     method: "GET",
-    action: "유저 댓글 목록 조회"
+    action: "유저별 댓글 목록 조회"
   },
   {
     endpoint: "/users/:id/books",
     url: "/users/1/books",
     method: "GET",
-    action: "유저 책 목록 조회"
+    action: "유저별 책 목록 조회"
   },
   {
     endpoint: "/users/:id/reviews",
     url: "/users/1/reviews",
     method: "GET",
-    action: "유저 리뷰 목록 조회"
+    action: "유저별 리뷰 목록 조회"
   },
   { endpoint: "/users", method: "POST", action: "유저 생성" },
   {
@@ -75,13 +75,13 @@ export const API_ENDPOINT_DATA = [
     endpoint: "/todos?userId={userId}",
     url: "/todos?userId=1",
     method: "GET",
-    action: "유저 할 일 목록 필터링"
+    action: "유저별 할 일 목록 조회"
   },
   {
     endpoint: "/todos?page={page}&limit={limit}",
     url: "/todos?page=1&limit=10",
     method: "GET",
-    action: "할 일 목록 페이지"
+    action: "할 일 목록 페이징"
   },
   { endpoint: "/todos", method: "POST", action: "할 일 생성" },
   {
@@ -115,7 +115,7 @@ export const API_ENDPOINT_DATA = [
     endpoint: "/posts?page={page}&limit={limit}",
     url: "/posts?page=1&limit=10",
     method: "GET",
-    action: "게시물 목록 페이지"
+    action: "게시물 목록 페이징"
   },
   {
     endpoint: "/posts/:id/comments",
@@ -127,7 +127,7 @@ export const API_ENDPOINT_DATA = [
     endpoint: "/posts?userId={userId}",
     url: "/posts?userId=1",
     method: "GET",
-    action: "유저 게시물 목록 필터링"
+    action: "유저별 게시물 목록 조회"
   },
   { endpoint: "/posts", method: "POST", action: "게시물 생성" },
   {
@@ -161,19 +161,19 @@ export const API_ENDPOINT_DATA = [
     endpoint: "/comments?page={page}&limit={limit}",
     url: "/comments?page=1&limit=10",
     method: "GET",
-    action: "댓글 목록 페이지"
+    action: "댓글 목록 페이징"
   },
   {
     endpoint: "/comments?userId={userId}",
     url: "/comments?userId=1",
     method: "GET",
-    action: "유저 댓글 목록 필터링"
+    action: "유저별 댓글 목록 조회"
   },
   {
     endpoint: "/comments?postId={postId}",
     url: "/comments?userId=1",
     method: "GET",
-    action: "게시물 댓글 목록 필터링"
+    action: "게시물 댓글 목록 조회"
   },
   { endpoint: "/comments", method: "POST", action: "댓글 생성" },
   {
@@ -202,7 +202,7 @@ export const API_ENDPOINT_DATA = [
     endpoint: "/books?page={page}&limit={limit}",
     url: "/books?page=1&limit=10",
     method: "GET",
-    action: "책 목록 페이지"
+    action: "책 목록 페이징"
   },
   {
     endpoint: "/books/:id/reviews",
@@ -237,19 +237,19 @@ export const API_ENDPOINT_DATA = [
     endpoint: "/reviews?page={page}&limit={limit}",
     url: "/reviews?page=1&limit=10",
     method: "GET",
-    action: "리뷰 목록 페이지"
+    action: "리뷰 목록 페이징"
   },
   {
     endpoint: "/reviews?userId={userId}",
     url: "/reviews?userId=1",
     method: "GET",
-    action: "유저 리뷰 필터링"
+    action: "유저별 리뷰 조회"
   },
   {
     endpoint: "/reviews?bookId={bookId}",
     url: "/reviews?bookId=1",
     method: "GET",
-    action: "책 리뷰 필터링"
+    action: "책별 리뷰 조회"
   },
   { endpoint: "/reviews", method: "POST", action: "리뷰 생성" },
   {
@@ -308,3 +308,39 @@ export const API_ENDPOINT_DATA = [
     action: "한글 로렘 입숨 생성"
   }
 ];
+
+export const ID_ENDPOINT_DATA = API_ENDPOINT_DATA.filter(
+  (data) => data.endpoint.endsWith("/:id") && data.method === "GET"
+);
+
+export const LIST_ENDPOINT_DATA = API_ENDPOINT_DATA.filter((data) =>
+  data.action.endsWith("목록")
+);
+
+export const PAGING_ENDPOINT_DATA = API_ENDPOINT_DATA.filter((data) =>
+  data.action.endsWith("페이징")
+);
+
+export const POST_ENDPOINT_DATA = API_ENDPOINT_DATA.filter(
+  (data) => data.method === "POST" && data.action !== "로그인"
+);
+
+export const PUT_ENDPOINT_DATA = API_ENDPOINT_DATA.filter(
+  (data) => data.method === "PUT"
+);
+
+export const PATCH_ENDPOINT_DATA = API_ENDPOINT_DATA.filter(
+  (data) => data.method === "PATCH"
+);
+
+export const DELETE_ENDPOINT_DATA = API_ENDPOINT_DATA.filter(
+  (data) => data.method === "DELETE"
+);
+
+export const FILTER_ENDPOINT_DATA = API_ENDPOINT_DATA.filter(
+  (data) => data.endpoint.includes("?") && data.action.includes("별")
+);
+
+export const NESTED_ENDPOINT_DATA = API_ENDPOINT_DATA.filter(
+  (data) => !data.endpoint.includes("?") && data.action.includes("별")
+);
