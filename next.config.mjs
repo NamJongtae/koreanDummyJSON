62bc2f35b1cdb9d4a8db1f58b8ad23fb7b532e21
NamJongtae <dark9737@gmail.com>
2026-02-04 23:23:38 +0900

    Refactor: Next 16 Turbopack에 맞춰 SVGR(SVG) 설정을 turbopack rules로 마이그레이션

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"]
-    });
-    return config;
+  turbopack: {
+     rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js"
+        }
+      }
   },
   images: {
     dangerouslyAllowSVG: true,
